8febdcb6810ecaa532fa911c2fffa52e9e8bf646
KOTA <user@USER-no-MacBook.local>
2019-05-14 13:40:39 +0700

    first commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws' # required
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAVGG2W7TBAJORUK5L',
-    aws_secret_access_key: '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', #開発環境においては使わない設定となっているので、今は必要ないです
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], #開発環境においては使わない設定となっているので、今は必要ないです
     region: 'ap-northeast-1',
     host:                  's3.amazonaws.com/geekphoto',             # optional, defaults to nil
     endpoint:              'https://s3.us-east-1.amazonaws.com' # optional, defaults to nil
